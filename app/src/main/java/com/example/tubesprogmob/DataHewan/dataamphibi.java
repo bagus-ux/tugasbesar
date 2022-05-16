6385363f24bf0b01c05f861fc164ad1c24fb4f6a
bagus-ux <bagusprayogo516@gmail.com>
2022-05-17 01:50:47 +0800

    tubes progmob

---
+++ app/src/main/java/com/example/tubesprogmob/DataHewan/dataamphibi.java
@@ -0,0 +1,40 @@
+package com.example.tubesprogmob.DataHewan;
+
+import com.example.tubesprogmob.R;
+
+public class dataamphibi {
+    public static String[] nama={
+            "Katak",
+            "Kodok",
+            "Biawak",
+            "Labi - Labi",
+            "Kura - Kura",
+            "Microsauria",
+            "Sesilia",
+            "Urodela",
+            "Axolotl"
+    };
+    public static String [] Keterangan={
+            "Katak adalah",
+            "Kodok adalah",
+            "Biawak adalah",
+            "Labi - Labi adalah",
+            "Kura - Kura adalah",
+            "Microsauria adalah",
+            "Sesilia adalah",
+            "Urodela adalah",
+            "Axolotl adalah"
+    };
+
+    public static Integer [] gambar={
+            R.drawable.katak,
+            R.drawable.kodok,
+            R.drawable.biawak,
+            R.drawable.labi,
+            R.drawable.kura,
+            R.drawable.microsauria,
+            R.drawable.sesilia,
+            R.drawable.urodela,
+            R.drawable.axolotl
+    };
+}
---
+++ app/src/main/java/com/example/tubesprogmob/DataHewan/datapisces.java
@@ -0,0 +1,5 @@
+package com.example.tubesprogmob.DataHewan;
+
+public class datapisces {
+
+}
